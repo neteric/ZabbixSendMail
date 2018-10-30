0b0ba7a6bb0ea6ed4ef855c9615fddd408e31faa
neteric <net.eric.zhang@gmail.com>
2018-10-30 17:06:44 +0800

    Update wechat.py
    fix token.json path problem

--- wechat.py
+++ wechat.py
@@ -4,12 +4,17 @@ import sys
 import urllib
 import json
 import fcntl
+import logging
 
-CorpId = "xxxxxxxxxx"
-Secret = "yyyyyyyyyy"
+CorpId = "xxxxxxxxx"
+Secret = "yyyyyyyyyyyyy"
 AgentId = "1000002"
 AccessTokenUri = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
 AppMsgSendUri = "https://qyapi.weixin.qq.com/cgi-bin/message/send"
+FORMAT = '%(asctime)s - %(message)s'
+logging.basicConfig(filename='/var/log/zabbix_alert.log',
+                    format=FORMAT,
+                    level=logging.NOTSET)
 
 
 class AccessTokenError(BaseException):
@@ -18,29 +23,34 @@ class AccessTokenError(BaseException):
 
 
 class WechatAlert(object):
-    def __init__(self, subject, content):
+    def __init__(self, sendto, subject, content):
+        self.sendto = sendto
         self.subject = subject
         self.content = content
+        logging.debug("sendto is:%s, subject is:%s, conent is %s" % (sendto, subject,content))
 
     def get_access_token(self):
         params = {"corpid": CorpId,
                   "corpsecret": Secret
                   }
         params = urllib.urlencode(params)
-        res = urllib.urlopen("%s?%s" % (AccessTokenUri,
-                                        params)).read()
+        try:
+            res = urllib.urlopen("%s?%s" % (AccessTokenUri,
+                                            params)).read()
         # TODO： check urlopen is right response
+        except Exception as err:
+            logging.debug(str(err))
 
         self.write_to_file(res)
 
     def write_to_file(self, access_token):
-        with open('token.json', 'w') as fd:
+        with open('/usr/lib/zabbix/alertscripts/token.json', 'w') as fd:
             # 加排他锁，其他进程既不能读也不能写
             fcntl.flock(fd.fileno(), fcntl.LOCK_EX)
             fd.write(access_token)
 
     def get_from_file(self):
-        with open('token.json', 'a+') as fd:
+        with open('/usr/lib/zabbix/alertscripts/token.json', 'a+') as fd:
             # 加共享锁,多个进程可以同时读，但其他进程不能写
             fcntl.flock(fd.fileno(), fcntl.LOCK_SH)
             fd.seek(0)
@@ -51,6 +61,7 @@ class WechatAlert(object):
 
     def send_message(self):
         access_token = self.get_from_file().get("access_token")
+        logging.debug("access_token is : %s" % (access_token))
         params = {"touser": "@all",
                   "toparty": "@all",
                   "totag": "@all",
@@ -58,14 +69,17 @@ class WechatAlert(object):
                   "agentid": AgentId,
                   "text": {"content": self.content},
                   }
-        res = urllib.urlopen("%s?access_token=%s"
-                             % (AppMsgSendUri,
-                                access_token),
-                             data=json.dumps(params)).read()
+        try:
+            res = urllib.urlopen("%s?access_token=%s"
+                                 % (AppMsgSendUri,
+                                    access_token),
+                                 data=json.dumps(params)).read()
+        except Exception as err:
+            logging.error(err)
         errcode = json.loads(res).get('errcode')
-        if errcode == 40014:
+        logging.debug("params:%s,res:%s" % (repr(params), res))
+        if errcode == 40014 or errcode == 42001:
             raise AccessTokenError
-        print(errcode)
 
     def main(self):
         try:
@@ -73,10 +87,13 @@ class WechatAlert(object):
         except AccessTokenError:
             self.get_access_token()
             self.send_message()
+        except Exception as err:
+            logging.error(str(err))
 
 
 if __name__ == '__main__':
     sys.exit(WechatAlert(
         sys.argv[1],
         sys.argv[2],
+        sys.argv[3],
     ).main())
